75d53cb1cae72e521e497b8585d1cdf4c6fc54e6
Kedesdt <kedesdt@gmail.com>
2026-09-22 22:23:36 -0300

    mudanca na porta padrao

--- app.py
+++ app.py
@@ -121,6 +121,6 @@ if __name__ == "__main__":
     socketio.run(
         app,
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", "5000")),
+        port=int(os.environ.get("PORT", "5555")),
         debug=True,
     )
